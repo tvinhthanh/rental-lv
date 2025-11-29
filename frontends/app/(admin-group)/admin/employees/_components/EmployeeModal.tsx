699acec13e82224dd307ef5809b8caa5a93305ea
trunghieuuu <trnughieu0608@gmail.com>
2025-11-30 03:56:04 +0700

    fix employees

--- frontends/app/(admin-group)/admin/employees/_components/EmployeeModal.tsx
+++ frontends/app/(admin-group)/admin/employees/_components/EmployeeModal.tsx
@@ -22,7 +22,10 @@ export default function EmployeeModal({ mode, data, onClose, onSuccess }: any) {
     });
 
     useEffect(() => {
-        branchService.getAll().then(setBranches);
+        branchService.getAll().then((res: any) => {
+            const items = Array.isArray(res) ? res : res?.items ?? [];
+            setBranches(items);
+        }).catch(() => setBranches([]));
     }, []);
 
     useEffect(() => {
